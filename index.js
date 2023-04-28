6a8ec63e167ea3f92e45673381a4e3ec6e869efe
rania <raniabm21@gmail.com>
2023-04-28 21:35:18 +0500

    installed EJS engine & added dummy files to check

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express")
 const app = express()
-
+const path = require('path')
 const hostname = '127.0.0.1'
 const port = 3000
 
 const userroute = require('./routes/notesRouter')
 
-app.use('/user', userroute)
+// //to show static html page in express js
+// //built-in middleware 
+// const staticPath = path.join(__dirname ,'/view')
+// app.use(express.static(staticPath))
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        name: "Rania"
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        name: "about"
+    })
+})
+
+app.use('/', userroute)
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
